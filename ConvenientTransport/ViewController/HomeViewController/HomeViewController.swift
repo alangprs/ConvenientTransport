228c30dacfb71ecd2f3a4603e2c1cdbe2929f0bb
alangprs <alangprs1@gmail.com>
2023-02-28 11:52:21 +0800

    feat: 修改 root window

--- ConvenientTransport/ViewController/HomeViewController/HomeViewController.swift
+++ ConvenientTransport/ViewController/HomeViewController/HomeViewController.swift
@@ -7,11 +7,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupUI()
+    }
+    
+    private func setupUI() {
+        view.backgroundColor = .systemRed
     }
 
 
